1a2f302b7aed9a83d13768760c327b2067d0d763
yeyuanhua <1378476425@qq.com>
2023-03-24 17:57:14 +0800

    bug

--- src/views/music/dj/index.vue
+++ src/views/music/dj/index.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import Banner from "./comps/banner.vue";
 import Show from "./comps/show.vue";
-// import DjCateList from "./comps/djCateList.vue";
+import CateList from "./comps/cateList.vue";
 import Rank from "./comps/rank.vue";
 import Anchor from "./comps/anchor.vue";
 </script>
 <template>
   <div>
     <Banner />
-    <!-- <DjCateList  /> -->
+    <CateList  />
     <Show />
     <Rank />
     <Anchor />
